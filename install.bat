1458a62923bd2659d50b33cf2dbfd5bca8597ffb
Lehman Black <dosnlinux@gmail.com>
2012-08-26 14:28:08 -0500

    changed %TMP% to %MYTMP%
    this allowed the removal of several comments that cluttered the code.
    moved other bits of code around to make it more tidy.

--- install.bat
+++ install.bat
@@ -9,15 +9,18 @@ set MYSQL_VERSION=5.5.16
 set MYSQL_USER=root
 set MYSQL_PASSWORD=
 
-set BUGZILLA_ANSWERS="answers.txt"
-set DB_SETUP_SCRIPT=%CD%\files\createdb.sql
+set BUGZILLA_ANSWERS=files\answers.txt
+set DB_SETUP_SCRIPT=files\createdb.sql
 
 set ADDON=Bugzilla
 
 set BIN=installer\bin
-set TMP=installer\temp
+REM using %MYTMP% in this script because install-module.pl fails to install
+REM some modules if %TMP% is changed from default.
+set MYTMP=installer\temp
 
 set WAMP=c:\wamp
+set WAMP_ALIAS=%WAMP%\alias
 set WAMP_APPS=%WAMP%\apps
 set WAMP_MYSQL=%WAMP%\bin\mysql\mysql%MYSQL_VERSION%\bin
 set WAMP_BUGZILLA=%WAMP_APPS%\bugzilla%BUGZILLA_VERSION%
@@ -35,84 +38,76 @@ set PATH=%PATH%;%BIN%;%WAMP_MYSQL%
 echo Welcome to the %ADDON% Addon installer for WampServer %WAMP_VERSION%
 
 REM set up the temp directory
-IF NOT EXIST %TMP% GOTO MKTMP
+IF NOT EXIST %MYTMP% GOTO MKTMP
 echo 	Temp directory found from previous install: DELETING
-rd /S /Q %TMP%
+rd /S /Q %MYTMP%
 
 :MKTMP
 echo 	Setting up the temp directory...
-mkdir %TMP%
+mkdir %MYTMP%
+pause
 
 REM download Bugzilla archive to temp directory
 echo 	Downloading %ADDON% binaries to temp directory...
-wget.exe -nd -q -P %TMP% %BUGZILLA_DOWNLOAD%
+wget.exe -nd -q -P %MYTMP% %BUGZILLA_DOWNLOAD%
+pause
 
 REM unzip the downloaded source files and install them
 echo 	Extracting the files from the downloaded archive...
-gzip.exe -d %TMP%\%BUGZILLA_FILE%.tar.gz
-REM tar and the -C switch doesn't want to work with %TMP%. currently treats
+gzip.exe -d %MYTMP%\%BUGZILLA_FILE%.tar.gz
+REM FIXME tar and the -C switch doesn't want to work with %MYTMP%. currently treats
 REM installer\temp as installer + \t + emp
-tar.exe -xf %TMP%\%BUGZILLA_FILE%.tar -C "installer\\temp"
-ren %TMP%\%BUGZILLA_FILE% %BUGZILLA_DIR%
+tar.exe -xf %MYTMP%\%BUGZILLA_FILE%.tar -C "installer\\temp"
+pause
 
 REM install the binary files in the WampServer install directory
 echo 	Moving the files to the WampServer install directory...
-move %TMP%\%BUGZILLA_DIR% %WAMP%\apps
+ren %MYTMP%\%BUGZILLA_FILE% %BUGZILLA_DIR%
+move %MYTMP%\%BUGZILLA_DIR% %WAMP_APPS%
+pause
 
 REM install the apache config file for Bugzilla
-REM FIXME: may be able to skip moving to%TMP% and just copy to %WAMP_APPS%
+REM FIXME: may be able to skip moving to %MYTMP% and just copy to %WAMP_APPS%
 REM FIXME: and rename during that copy command
 echo 	Installing %ADDON% configuration files...
-copy wamp\alias\%BUGZILLA_ALIAS% %WAMP%\alias
-REM FIXME: having trouble getting paths with spaces being recognized by
-REM FIXME: input redirection, so I will copy the answers file to %WAMP_BUGZILLA%
-REM FIXME: as a workaround. Path origionally used was %PROFILE%\desktop\wamp-bugzilla-addon
-copy files\%BUGZILLA_ANSWERS% %WAMP_BUGZILLA%
+copy wamp\alias\%BUGZILLA_ALIAS% %WAMP_ALIAS%
 pause
 
 REM install extra perl modules needed by Bugzilla
 REM FIXME: this needs cleaned up: this installs some extras that aren't
-REM really needed (db drivers and such) maybe have a minimal install list
-REM of missing modules and install them and then a full which is everything
-REM except the unneccessary db modules. Also need to better understand
-REM what's going on so that packaging/removal is easier. may even consider
-REM adding this to it's own installer.
-REM FIXME: this cd line is needed. calling install-module.pl from the addon
-REM directory causes exceptions to be thrown
+REM FIXME really needed (db drivers and such) maybe have a minimal install list
+REM FIXME of missing modules and install them and then a full which is everything
+REM FIXME except the unneccessary db modules. Also need to better understand
+REM FIXME what's going on so that packaging/removal is easier.
+
+REM FIXME install-module.pl will not work unless in the bugzilla install directory. might need to open bug
 cd %WAMP_BUGZILLA%
-REM FIXME: for some weird reason the install-module.pl script fails to detect
-REM FIXME: the compiler and can't install some modules because we have changed %TMP%
-REM FIXME: need to figure out why this occurs
-set TMP=%TEMP%
+REM FIXME: install-module.pl fails to install some modules because we changed %TMP%. need to figure out why this occurs or open bug with bugzilla
 perl install-module.pl --all
+cd %~dp0%
+pause
 
 REM setup MySQL database/user for Bugzilla
 REM FIXME: automated login does not work. still getting prompted
-mysql -u %MYSQL_USER% -p "%MYSQL_PASSWORD%" < "%DB_SETUP_SCRIPT%"
+mysql -u %MYSQL_USER% -p "%MYSQL_PASSWORD%" < %DB_SETUP_SCRIPT%
 
 REM configuring bugzilla
 REM FIXME: checksetup needs to be run at least once so that localconfig can
 REM FIXME: can be generated. this might be skipped by just copying a default
 REM FIXME: localconfig and then only having to run checksetup.pl once
-perl checksetup.pl --check-modules
-perl checksetup.pl < %WAMP_BUGZILLA%\%BUGZILLA_ANSWERS%
+perl %WAMP_BUGZILLA%\checksetup.pl --check-modules
+perl %WAMP_BUGZILLA%\checksetup.pl < %BUGZILLA_ANSWERS%
 pause
 
-REM fix shbang on cgi and pl files to work better with windows cgi without
-REM requiring registry edits
+REM fix shbang so windows can guess CGI interpreter without requiring registry edits
 REM FIXME clean up this regex
-REM FIXME windows perl can not do inplace edits without backup need to delete backup files after
-for /R . %%i in (*.cgi *.pl) do 	perl -p -i'.bak' -e "s@/usr/bin/@@" %%i
+REM FIXME windows perl can not do inplace edits without backup need to delete backup files after. need to delete those
+for /R %WAMP_BUGZILLA% %%i in (*.cgi *.pl) do perl -p -i'.bak' -e "s@/usr/bin/@@" %%i
 pause
 
 REM clean up temp files
 echo 	Cleaning up temp files...
-REM FIXME set %TMP% back to our value so that we don't accidently delete the system %TMP% dir
-REM FIXME this is to fix a bug relating to the install-module.pl script needing %TMP% needing
-REM FIXME to be set to the system value so that certain modules get built.
-REM rd /S /Q %TMP%
-REM del %WAMP_BUGZILLA%\%BUGZILLA_ANSWERS%
-cd %~dp0%
+rd /S /Q %MYTMP%
 
 echo %ADDON% is installed successfully. Please restart WampServer.
 
